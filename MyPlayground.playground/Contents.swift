63ce60507034221bb9eee2d07d4c0685475ff1fe
packtbipin2377 <44016603+packtbipin2377@users.noreply.github.com>
2019-08-08 11:55:05 +0530

    Code Added
    Code Added

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,23 @@
+// Loops
+
+for number in 1...10 {
+    print("I love loops! \(number)")
+}
+
+var favDrinks = ["Baja Blast", "Root Beer", "Pepsi"]
+
+for index in 0...2 {
+    print(favDrinks[index])
+}
+
+for drink in favDrinks {
+    print(drink)
+}
+
+// Make an array of Ints
+var ints = [321,4,125,32,5,24,6,35,6,32]
+// Make a for loops that goes through each int, multiplies it by 2 and prints the result
+for int in ints {
+    print(int * 2)
+}
+
